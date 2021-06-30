d8e2df16263945005db0bdaf49912356de691f20
thedr <andongtbvx@gmail.com>
2021-06-30 21:12:24 +0700

    delete config

--- config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
-
-
-module.exports = new Sequelize("forum", "root", process.env.DB_PW, {
-	host: "localhost",
-	dialect: "mysql"
-});
-
