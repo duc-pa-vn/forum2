6e7790fe75ef577bf8d442c97ac6cdb1cf645c2b
juno <cuongjunocn1@gmail.com>
2021-06-30 18:35:34 +0700

    change structrue folder + add eslint + add sequelize  migrate

---
+++ migrations/user.js
@@ -0,0 +1,37 @@
+"use strict";
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable("user", {
+			id: {
+				type: Sequelize.INTEGER,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			email: {
+				type: Sequelize.STRING,
+			},
+			hash_password: {
+				type: Sequelize.STRING,
+			},
+			full_name: {
+				type: Sequelize.STRING,
+			},
+			nickname: {
+				type: Sequelize.STRING,
+			},
+			active: {
+				type: Sequelize.BOOLEAN,
+			},
+			avatar: {
+				type: Sequelize.STRING,
+			},
+			create_at: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface) => {
+		await queryInterface.dropTable("User");
+	},
+};
